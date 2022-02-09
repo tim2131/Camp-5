b835fa9349acccd1e770a22f89d3113ad9429224
vm9487 <vm9487@hotmail.com>
2022-02-09 10:38:10 +0800

    T/pilot run for antd/20220209/10:38

--- Camp-fe-center/src/App.js
+++ Camp-fe-center/src/App.js
@@ -1,10 +1,11 @@
 import './style/App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+<div className="App">
+    <Button type="primary">Button</Button>
+  </div>
   );
 }
 
