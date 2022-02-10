cfbca810e6d90c45ba7d656be655bf27a0f2c7b3
vm9487 <vm9487@hotmail.com>
2022-02-10 10:08:28 +0800

    T/modify theme/20220210 1007

--- member-center/craco.config.js
+++ member-center/craco.config.js
@@ -1,17 +1,25 @@
 const CracoLessPlugin = require('craco-less');
+const CracoAntDesignPlugin = require("craco-antd");
+const path = require('path'); 
 
 module.exports = {
   plugins: [
     {
-      plugin: CracoLessPlugin,
+
+      plugin: CracoAntDesignPlugin,
       options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            modifyVars: { '@primary-color': '#1DA57A' },
-            javascriptEnabled: true,
-          },
-        },
-      },
+      customizeThemeLessPath:path.join(__dirname,"antd_customize.less")}
+
+      // plugin: CracoLessPlugin,
+      // options: {
+      //   lessLoaderOptions: {
+      //     lessOptions: {
+      //       modifyVars: { '@primary-color': '#1DA57A' },
+      //       javascriptEnabled: true,
+      //     },
+      //   },
+      // },
+
     },
   ],
 };
