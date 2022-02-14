615fc5f8adb2f6c24a6d3282787b51bc17dc48e2
vm9487 <vm9487@hotmail.com>
2022-02-14 13:51:07 +0800

    T/camp order list content done/20220214 1350

--- member-center/src/comp/CampOrder.js
+++ member-center/src/comp/CampOrder.js
@@ -14,7 +14,8 @@ const data = [
     orderdate_start: "2022/5/1",
     orderdate_end: "2022/5/3",
     order_status: "1",
-    tag_status: "1"
+    tag_status: "1",
+    tent_type: "",
   },
   {
     id: "2",
@@ -27,15 +28,17 @@ const data = [
     order_status: "2",
     tag_status: "2"
   },
-  {  id: "3",
-  camp: "綠色綠色綠色生活露營",
-  county: "新北市",
-  href: "",
-  pic: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  orderdate_start: "2022/5/1",
-  orderdate_end: "2022/5/3",
-  order_status: "3",
-  tag_status: "3" },
+  {
+    id: "3",
+    camp: "綠色綠色綠色生活露營",
+    county: "新北市",
+    href: "",
+    pic: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    orderdate_start: "2022/5/1",
+    orderdate_end: "2022/5/3",
+    order_status: "3",
+    tag_status: "3"
+  },
   {
     id: "4",
     camp: "綠色生活露營",
@@ -58,15 +61,17 @@ const data = [
     order_status: "2",
     tag_status: "2"
   },
-  {  id: "6",
-  camp: "綠色生活露營",
-  county: "新北市",
-  href: "",
-  pic: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  orderdate_start: "2022/5/1",
-  orderdate_end: "2022/5/3",
-  order_status: "3",
-  tag_status: "3" },
+  {
+    id: "6",
+    camp: "綠色生活露營",
+    county: "新北市",
+    href: "",
+    pic: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    orderdate_start: "2022/5/1",
+    orderdate_end: "2022/5/3",
+    order_status: "3",
+    tag_status: "3"
+  },
 ];
 
 
@@ -96,12 +101,12 @@ const CampOrder = () => {
 
   return (
     <List
-    
-     size="small"
+
+      size="small"
       itemLayout="horizontal"
       dataSource={data}
       pagination={{
-        position:"bottom",
+        position: "bottom",
         onChange: (page) => {
           console.log(page);
         },
@@ -109,49 +114,52 @@ const CampOrder = () => {
       }}
       renderItem={(item) => (
         < div className="list">
-        <List.Item
-        
-          actions={[
-            <a key="list-loadmore-edit">訂單詳細</a>,
-            <a key="list-loadmore-more">給予評論</a>,
-          ]}
-          key={item.id}
-        //   extra={          }
-        >
-          <List.Item.Meta
-            title={<>
-              <div className={orderStatuscolor[item.order_status]}>
-              {orderStatus[item.order_status]}
-              </div>
-              <a className="campTitle" href={item.href}>
-                {item.camp}
-              </a>
+          <List.Item
 
-            </>
+            actions={[
+            ]}
+            key={item.id}
+          //   extra={          }
+          >
+            <List.Item.Meta
+              title={<>
+                <div className={orderStatuscolor[item.order_status]}>
+                  {orderStatus[item.order_status]}
+                </div>
+                <a className="campTitle" href={item.href}>
+                  {item.camp}
+                </a>
 
-            }
-            description={
-              <>
-                <span className="campdate">{item.orderdate_start}</span>~
-                <span className="campdate">{item.orderdate_end}</span>
-                <br />
-                <span className="campdate">{item.county}</span>
               </>
-            }
-          />
 
-          {/* --------------pic-------------------- */}
-          <div className="orderPicBox">
-            <div className="tagWord">
-              {tagWords[item.order_status]}
+              }
+              description={
+                <>
+                  <span className="campdate">{item.orderdate_start}</span>~
+                  <span className="campdate">{item.orderdate_end}</span>
+                  <br />
+                  <span className="campdate">{item.county}</span>
+                  
+                  <div className="orderlinkbox">
+                  <a className="orderlinks" key="list-loadmore-edit">訂單詳細</a> |  
+                  <a className="orderlinks" key="list-loadmore-more">給予評論</a>
+                  </div>
+                </>
+              }
+            />
+
+            {/* --------------pic-------------------- */}
+            <div className="orderPicBox">
+              <div className="tagWord">
+                {tagWords[item.order_status]}
+              </div>
+              <div className={tagcolor[item.order_status]}></div>
+              <div className="list_item">
+                <img className="pic" src={item.pic} alt="camp-pic" />
+              </div>
             </div>
-            <div className={tagcolor[item.order_status]}></div>
-            <div className="list_item">
-              <img className="pic" src={item.pic} alt="camp-pic" />
-            </div>
-          </div>
-          {/* ----------------------------- */}
-        </List.Item>
+            {/* ----------------------------- */}
+          </List.Item>
         </div>
       )}
     />
