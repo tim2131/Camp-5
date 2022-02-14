80e3d0400309bbe860d31634e6d389ec2c6031fc
vm9487 <vm9487@hotmail.com>
2022-02-14 12:05:43 +0800

    T/update footer and camp order list/20220214 12 05

--- member-center/src/comp/CampOrder.js
+++ member-center/src/comp/CampOrder.js
@@ -18,6 +18,37 @@ const data = [
   },
   {
     id: "2",
+    camp: "綠色生dddddddd活露營",
+    county: "新北市",
+    href: "",
+    pic: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    orderdate_start: "2022/5/1",
+    orderdate_end: "2022/5/3",
+    order_status: "2",
+    tag_status: "2"
+  },
+  {  id: "3",
+  camp: "綠色綠色綠色生活露營",
+  county: "新北市",
+  href: "",
+  pic: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  orderdate_start: "2022/5/1",
+  orderdate_end: "2022/5/3",
+  order_status: "3",
+  tag_status: "3" },
+  {
+    id: "4",
+    camp: "綠色生活露營",
+    county: "桃園縣",
+    href: "",
+    pic: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    orderdate_start: "2022/5/1",
+    orderdate_end: "2022/5/3",
+    order_status: "1",
+    tag_status: "1"
+  },
+  {
+    id: "5",
     camp: "綠色生活露營",
     county: "新北市",
     href: "",
@@ -27,29 +58,50 @@ const data = [
     order_status: "2",
     tag_status: "2"
   },
-  { title: "Ant Design Title 3" },
-  { title: "Ant Design Title 4" },
+  {  id: "6",
+  camp: "綠色生活露營",
+  county: "新北市",
+  href: "",
+  pic: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  orderdate_start: "2022/5/1",
+  orderdate_end: "2022/5/3",
+  order_status: "3",
+  tag_status: "3" },
 ];
 
 
 
 const CampOrder = () => {
 
-const tagWords={
-  1:"主打",
-  2:"促銷",
-}
+  const tagWords = {
+    1: "主打",
+    2: "促銷",
+  }
 
   const tagcolor = {
     1: "tagStar",
     2: "tag",
-};
+  };
+
+  const orderStatus = {
+    1: "未完成",
+    2: "完成",
+    3: "取消",
+  };
+  const orderStatuscolor = {
+    1: "statusTagTBD",
+    2: "statusTagDone",
+    3: "statusTagCancel",
+  };
 
   return (
     <List
+    
+     size="small"
       itemLayout="horizontal"
       dataSource={data}
       pagination={{
+        position:"bottom",
         onChange: (page) => {
           console.log(page);
         },
@@ -58,35 +110,40 @@ const tagWords={
       renderItem={(item) => (
         <List.Item
           actions={[
-            <a key="list-loadmore-edit">給予評論</a>,
-            <a key="list-loadmore-more">訂單詳細</a>,
+            <a key="list-loadmore-edit">訂單詳細</a>,
+            <a key="list-loadmore-more">給予評論</a>,
           ]}
           key={item.id}
         //   extra={          }
         >
           <List.Item.Meta
-            title={<a href={item.href}>{item.camp}</a>}
+            title={<>
+              <div className={orderStatuscolor[item.order_status]}>
+              {orderStatus[item.order_status]}
+              </div>
+              <a className="campTitle" href={item.href}>
+                {item.camp}
+              </a>
+
+            </>
+
+            }
             description={
               <>
-                <span>{item.orderdate_start}</span>~
-                <span>{item.orderdate_end}</span>
+                <span className="campdate">{item.orderdate_start}</span>~
+                <span className="campdate">{item.orderdate_end}</span>
                 <br />
-                <span>{item.county}</span>
+                <span className="campdate">{item.county}</span>
               </>
             }
           />
-          {/* -----order--status--tag-------------- */}
-          <div className="statusTag">{item.order_status}</div>
-          {/* ---------pic-------------------- */}
+
+          {/* --------------pic-------------------- */}
           <div className="orderPicBox">
-
-
             <div className="tagWord">
-            {tagWords[item.order_status]}
+              {tagWords[item.order_status]}
             </div>
             <div className={tagcolor[item.order_status]}></div>
-
-
             <div className="list_item">
               <img className="pic" src={item.pic} alt="camp-pic" />
             </div>
