99b671aacc5d8af4a9b70f49b78448a561ff83f0
tim2131 <tim123452131@gmail.com>
2022-02-15 01:59:53 +0800

    react 首頁

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/navbar/navbar';
+import Home from './pages/home'
+// import Footer from "./components/Footer";
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
