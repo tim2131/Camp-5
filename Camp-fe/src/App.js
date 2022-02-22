957b6c41e7a17ae041cc53e87947c20dc1b4fa25
hyh <vm9487@hotmail.com>
2022-02-22 21:08:37 +0800

    T/add comment and stars is able to send to the backend/20220222 2108

--- Camp-fe/src/App.js
+++ Camp-fe/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Navbar from './components/navbar/navbar';
 import Home from './pages/home'
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login"
 
 function App() {
   
   return (
     <>
-     <Navbar />
-     <Home />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Home />
+            </>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
